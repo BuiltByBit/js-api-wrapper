3001b5479142169bca37d3b24eb8ffb9399425b4
Harry <hello@majored.pw>
2022-01-16 00:18:07 +0000

    Update SortOptions tests to use `assert` module

--- tests/sort_options.js
+++ tests/sort_options.js
@@ -1,33 +1,25 @@
 // Copyright (c) 2021 MC-Market (Mick Capital Pty. Ltd.)
 // MIT License (https://github.com/MC-Market-org/js-api-wrapper/blob/main/LICENSE)
 
+const assert = require("assert");
 const { SortOptions } = require("../src/SortOptions.js");
 
 function single_sort() {
     let options = new SortOptions();
     options.sort = "purchases";
-
-    if (options.toQueryString() !== "?sort=purchases") {
-        throw "single_sort() failed - expected did not match actual.";
-    }
+    assert.strictEqual(options.toQueryString(), "?sort=purchases");
 }
 
 function single_order() {
     let options = new SortOptions();
     options.order = "asc";
-
-    if (options.toQueryString() !== "?order=asc") {
-        throw "single_order() failed - expected did not match actual.";
-    }
+    assert.strictEqual(options.toQueryString(), "?order=asc");
 }
 
 function single_page() {
     let options = new SortOptions();
     options.page = 5;
-
-    if (options.toQueryString() !== "?page=5") {
-        throw "single_page() failed - expected did not match actual.";
-    }
+    assert.strictEqual(options.toQueryString(), "?page=5");
 }
 
 single_sort();
