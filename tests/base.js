30bd4685da454490a6c1c883b5753521df67cd56
Harry <hello@majored.pw>
2022-01-16 00:00:43 +0000

    Setup test directory & linting check

---
+++ tests/base.js
@@ -0,0 +1,3 @@
+// Copyright (c) 2021 MC-Market (Mick Capital Pty. Ltd.)
+// MIT License (https://github.com/MC-Market-org/js-api-wrapper/blob/main/LICENSE)
+
---
+++ tests/test.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2021 MC-Market (Mick Capital Pty. Ltd.)
+// MIT License (https://github.com/MC-Market-org/js-api-wrapper/blob/main/LICENSE)
+
+require("./base.js");
+throw "Test";
