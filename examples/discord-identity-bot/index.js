1fad7b6a75cd860bd4ee64ab99d6a2ddf95819d9
Harry <hello@majored.pw>
2022-05-12 12:59:20 +0100

    Update index.js

--- examples/discord-identity-bot/index.js
+++ examples/discord-identity-bot/index.js
@@ -7,13 +7,13 @@ const { Client, Intents } = require('discord.js');
 const mcmToken = new Token(TokenType.PRIVATE, "Find @ https://www.mc-market.org/account/api");
 const botToken = "Find @ https://discord.com/developers/applications";
 
-const GUILD_ID = 0;
-const ROLE_ID = 0;
-const CHANNEL_ID = 0;
+const GUILD_ID = "0";
+const ROLE_ID = "0";
+const CHANNEL_ID = "0";
 
 let guild = undefined;
-let role = undefined;
 let channel = undefined;
+let role = undefined;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const wrapper = new Wrapper();
@@ -23,8 +23,8 @@ async function main() {
     await wrapper.init(mcmToken);
 
     guild = await client.guilds.fetch(GUILD_ID);
+    channel = await client.channels.fetch(CHANNEL_ID);
     role = await guild.roles.fetch(ROLE_ID);
-    channel = await guild.channels.fetch(CHANNEL_ID);
 
     client.on("guildMemberAdd", join);
 }
@@ -43,7 +43,9 @@ async function join(user) {
         throw error;
     }
 
-    await channel.send(`[MC-Market] A member was found for ${user}: '${member.username}' (${member.member_id}).`);
+    let member_url = `https://wwww.mc-market.org/members/${member.member_id}/`;
+    await channel.send(`[MC-Market] A member was found for ${user}: '${member.username}' (${member_url}).`);
+
     await user.roles.add(role);
 }
 
